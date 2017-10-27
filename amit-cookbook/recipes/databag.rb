b94ba6dfafd291742f80e3ad9ee1220dadf1ce30
Naudiyal <anaudiya@9801a7ab9fad.ant.amazon.com>
2017-10-27 15:17:48 +1100

    mynewcookbook

---
+++ amit-cookbook/recipes/databag.rb
@@ -0,0 +1,6 @@
+search("aws_opsworks_instance").each do |instances|
+  Chef::Log.info("********** This instance's instance ID is '#{instances['instance_id']}' **********")
+  Chef::Log.info("********** This instance's public IP address is '#{instances['public_ip']}' **********")
+  Chef::Log.info("********** This instance's status is  '#{instances['status']}' **********")
+end
+

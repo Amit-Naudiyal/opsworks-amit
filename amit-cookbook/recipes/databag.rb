206e5d3e6ed920d0885f27d76c4e2a43fc043860
Naudiyal <anaudiya@9801a7ab9fad.ant.amazon.com>
2017-10-27 15:40:07 +1100

    change

--- amit-cookbook/recipes/databag.rb
+++ amit-cookbook/recipes/databag.rb
@@ -1,4 +1,5 @@
 search("aws_opsworks_instance").each do |instances|
+  Chef::Log.info("********** This instance's status is  '#{instances['hostname']}' **********")
   Chef::Log.info("********** This instance's instance ID is '#{instances['instance_id']}' **********")
   Chef::Log.info("********** This instance's public IP address is '#{instances['public_ip']}' **********")
   Chef::Log.info("********** This instance's status is  '#{instances['status']}' **********")
